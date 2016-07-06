007f869c5e1fece5148e90002b483629707a643f
Bang Nguyen <bangnv77@gmail.com>
2016-07-06 07:49:47 -0700

    still try more codes

--- app.rb
+++ app.rb
@@ -8,6 +8,6 @@ get('/')do
   erb(:index)
 end
 get ('/word_count') do
-  @word_count = params.fetch('word').word_count(params.fetch('sentence'))
+  @word_count == params.fetch('word').word_count(params.fetch('sentence')).to_i
   erb(:word_count)
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,11 +1,9 @@
 require('rspec')
 require('./lib/word_count.rb')
 
-describe('String') do
-  describe('#word_count') do
-    It("will count the frequency of a word appearance in a sentence") do
-      expect("peck".word_count("If Peter Piper picked a peck of pickled peppers,
-      where is the peck of pickled peppers picked")).to(eq("2"))
-    end
+describe('String#word_count') do
+  it("counts the frequency of a word appearance in a sentence") do
+    expect("peck".word_count("If Peter Piper picked a peck of pickled peppers,
+    where is the peck of pickled peppers picked")).to(eq("2"))
   end
 end
