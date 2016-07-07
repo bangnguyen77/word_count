3df8c0ddeb599c989d6d2696e658a89e1545ad20
Bang Nguyen <bangnv77@gmail.com>
2016-07-07 16:24:37 -0700

    final fix to pass the rspec tests and have expected results

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,16 +1,11 @@
 class String
-  define_method(:word_count) do |sentence|
+  define_method(:word_count) do | sentence |
     counter = 0
-    sentence.downcase!().split().each() do |word|
-      if word == self
+    sentence.downcase().split().each() do |word|
+      if word == self.downcase()
         counter += 1
       end
     end
     counter
-    if counter == 0
-      return "The sentence doesn't have your word"
-    else
-      return counter
-    end
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,22 +3,22 @@ require('./lib/word_count.rb')
 
 describe('String#word_count') do
   it("counts one same single word") do
-    expect(("Hello").word_count("Hello")).to(eq(1))
+    expect("Hello".word_count("Hello")).to(eq(1))
   end
 
-  # it("counts a single word in a sentence") do
-  #   expect(("like").word_count("I like dogs")).to(eq(1))
-  # end
-  #
-  # it("counts multiple words in a sentence") do
-  #   expect(("dog").word_count("the dog jumps over another dog")).to(eq(2))
-  # end
-  #
-  # it("counts words despite any capitalization") do
-  #   expect(("dog").word_count("The DOG jumped over the dog")).to(eq(2))
-  # end
-  #
-  # it("recognizes the word doesn't exist in the sentence") do
-  #   expect(("fox").word_count("the dog jumps over another dog")).to(eq(0))
-  # end
+  it("counts a single word in a sentence") do
+    expect("like".word_count("I like frogs")).to(eq(1))
+  end
+
+  it("counts multiple words in a sentence") do
+    expect("like".word_count("I like frogs like alot")).to(eq(2))
+  end
+
+  it("counts words despite any capitalization") do
+    expect("dog".word_count("The DOG jumped over the dog")).to(eq(2))
+  end
+
+  it("recognizes the word does not exist in the sentence") do
+    expect("dog".word_count("the dog jumps over another dog")).to(eq(2))
+  end
 end
